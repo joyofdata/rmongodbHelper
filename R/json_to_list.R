6ddd70d42638d02d44c5070468f4ec12f4df3d1c
Raffael <raffael.vogler.de@gmail.com>
2014-08-24 15:25:05 +0200

    remove newlines, convert booleans, no more .ARR=1

--- R/json_to_list.R
+++ R/json_to_list.R
@@ -7,13 +7,15 @@
 #' @export
 json_to_list <- function(json) {
   
+  json <- gsub("\n","",json)
   json <- gsub("\\{\\}","list()",json)
   json <- gsub("\\[\\]","list(.ARR=1)",json)
   json <- gsub("\\{","list(",json)
   json <- gsub("\\}",")",json)
   json <- gsub(":","=",json)
-  json <- gsub("\\[","list(.ARR=1,",json)
   json <- gsub("\\]",")",json)
+  json <- gsub("true","TRUE",json)
+  json <- gsub("false","FALSE",json)
   
   return(eval(parse(text = json)))
 }
